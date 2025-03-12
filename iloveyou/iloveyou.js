82d90b3d286eb02d19b5db23fc695fab8a0ab70c
unknown <rjmeldogello6@gmail.com>
2025-03-12 22:36:26 +0800

    iloveyou1

---
+++ iloveyou/iloveyou.js
@@ -0,0 +1,21 @@
+var girl = 143;
+var boy = 123;
+
+
+
+while (girl == 143){
+
+    girl = prompt("Enter The Secret Code:");
+
+
+    if (boy == 123 || girl == 143 ){
+        alert("I hate You")
+    }
+
+
+
+
+
+
+}
+
