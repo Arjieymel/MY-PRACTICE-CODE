d944bac5df8c4d26fc6cf63a9389d14da23f7c36
unknown <rjmeldogello6@gmail.com>
2025-03-28 21:06:33 +0800

    practice

---
+++ iloveyou/name.js
@@ -0,0 +1,8 @@
+function printName(f_name, l_name) {
+    fullName = f_name + " " + l_name; 
+    completeName = "My Full name is " + fullName; 
+    return completeName; 
+}
+
+var name = printName("Rj-Mel", "Dogello");
+console.log(name); 
